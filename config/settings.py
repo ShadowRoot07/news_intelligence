fcdb19bd1845ab5759db0d572ef8153b76322d0d
ShadowRoot07 <angstart.1109@gmail.com>
2026-03-16 17:37:48 +0000

    Deploy: try 1

--- config/settings.py
+++ config/settings.py
@@ -45,10 +45,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     'news_aggregator',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
